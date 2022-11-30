70bb61277ece9a55f3ef1d5668fdadc5a64b27e2
Nilodirf <106656383+Nilodirf@users.noreply.github.com>
2022-11-30 11:18:25 +0100

    asd

---
+++ FGT/Phonon DOS/PDOS.py
@@ -0,0 +1,179 @@
+import numpy as np
+import math
+import os
+from matplotlib import pyplot as plt
+from scipy import interpolate
+from scipy import constants as sp
+
+
+#define grids for cp computation
+E_new=np.arange(0, 50+1e-4, 1e-4)
+Ts=np.arange(1,1000,1)
+
+
+#prepare some variables for the DOS computation from the band spectra
+deltE=5e-1
+deltEJ=deltE*sp.electron_volt
+deltk=1e-4
+V_unit_cell=3.9*3.9*16.3*1e-30
+
+def prep_Es(dE):
+    E_range=np.arange(0, 50+dE, dE)
+    return(E_range)
+
+#get the grids for DOS computation:
+E_range= prep_Es(deltE)
+
+def k_discrete(dk):
+    return np.arange(0.,16., dk)
+
+k_disc=k_discrete(deltk)
+
+def E_discrete(E, kspace, E_round_decimal):
+    E_discrete=np.round_(E(kspace), E_round_decimal)
+    return(E_discrete)
+
+
+def Bose_Einstein(T, E):
+    bef=1/(np.exp(E/sp.k/T[:, np.newaxis])-1)
+    return(bef)
+
+
+def get_bandstructure(method):
+    band_files = os.listdir('C:/Users/tgrie/OneDrive/Dokumente/GitHub/3TM_Data/FGT/Phonon DOS/bands')
+    band_list=[]
+    ac_band_list = []
+    op_band_list = []
+    for bf in band_files:
+        b_s=str(bf)
+        dat=open('bands/'+ str(bf), 'r').readlines()[:-1]
+        dat_no_lineskips=[line.replace('\n', '') for line in dat]
+        dat_singled=[line.split(';') for line in dat_no_lineskips]
+        raw_ks=[float(line[0].replace(',', '.')) for line in dat_singled]
+        ks=math.pi/3.9e-10*np.array(raw_ks)*1e-9    #rad/nm  #time math.pi because band data is saved on scale [0,2]
+        raw_es=[float(line[1].replace(',', '.')) for line in dat_singled]
+        es=sp.hbar*2*math.pi*sp.c*np.array(raw_es)/sp.electron_volt*100*1000   #fist from J to eV, then from 1/cm to 1/m, then from eV to meV
+        if ks[-1]<16:
+            ks[-1]=16
+        if ks[0]>0:
+            ks[0]=0
+        f=interpolate.interp1d(ks, es)
+        E_disc = E_discrete(f, k_disc, 4)
+        if method== 'opac':
+            if b_s.endswith('17.txt') or b_s.endswith('18.txt') or b_s.endswith('16.txt'):
+                ac_band_list.append(E_disc)
+            else:
+                op_band_list.append(E_disc)
+        elif method == 'cutoff':
+            band_list.append(E_disc)
+    if method=='cutoff':
+            return band_list
+    if method=='opac':
+            return ac_band_list, op_band_list
+
+def get_DOS(bands, dE):
+    E_hist=prep_Es(deltE)
+    for band in bands:
+        for point in band:
+            E_hist[int(point*1/dE)]+=1
+    DOS=E_hist*deltk/deltE
+    return DOS
+
+def cp(dos, e, bef, dT):
+    ep=np.sum(e*dos*bef*deltE,axis=-1)/V_unit_cell
+    cp=np.diff(ep)/dT
+    return(cp)
+
+def cp_cutoff(DOS, cutoff):
+    cps=[]
+    E1 = E_range[1:int(cutoff)]*1e-3*sp.electron_volt
+    E2 = E_range[int(cutoff + 1):]*1e-3*sp.electron_volt
+    DOS1 = DOS[1:int(cutoff)]
+    DOS2 = DOS[int(cutoff + 1):]
+    Es = [E1, E2]
+    DOSs = [DOS1, DOS2]
+
+    for i, E in enumerate(Es):
+        cps.append(cp(DOSs[i], E, Bose_Einstein(Ts, E), 1))
+    return cps
+
+def cp_opac(DOSac, DOSop):
+    cph=[]
+    DOSs=[DOSac, DOSop]
+    E=E_range[1:]*1e-3*sp.electron_volt
+    for dos in DOS:
+        cph.append(cp(dos, E, Bose_Einstein(Ts, E), 1))
+    return cph
+
+
+def plot_bands_DOS(model, bands, DOS, bands_ac=None, bands_op=None, DOS_ac=None, DOS_op=None, cutoff=None):
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    if model=='cutoff':
+        for band in bands:
+            ax1.plot(k_disc, band, color='blue', lw=2.0)
+            ax2.plot(DOS[1:cutoff], E_range[1:cutoff], color='blue')
+            ax2.plot(DOS[cutoff + 1:], E_range[cutoff+1:], color='red')
+
+    elif model=='opac':
+        for band in bands_ac:
+            ax1.plot(k_disc, band, lw=2.0, color='orange')
+            ax2.plot(DOS_ac, E_range, color='orange')
+        for band in bands_op:
+            ax1.plot(k_disc, band, lw=2.0, color='purple')
+            ax2.plot(DOS_op, E_range, color='purple')
+        ax2.plot(DOS, E_range, color='black', label=r'total')
+
+    ax1.set_xlabel(r'k', fontsize=16)
+    ax1.set_ylabel(r'E [meV]', fontsize=16)
+    ax1.set_xlim(0, 16)
+    ax1.set_ylim(0, 50)
+    ax1.xaxis.set_ticklabels([])
+
+    ax2.set_xlim(0,)
+    ax2.sharey(ax1)
+    ax2.set_xlabel(r'DOS [states/eV]', fontsize=16)
+    plt.savefig('PDOS_cutoff_2.pdf')
+    plt.show()
+
+def plot_cp_opac(cpac, cpop):
+    plt.plot(Ts[:-1], cpac, label='accoustic phonons')
+    plt.plot(Ts[:-1], cpop, label='optical phonons')
+    plt.xlabel(r'Temperature [K]', fontsize=16)
+    plt.ylabel(r'Lattice heat capcity [J/m$^3$K]', fontsize=16)
+    plt.title(r'Heat capacity of different modes', fontsize=18)
+    plt.legend(fontsize=16)
+    plt.show()
+
+def plot_cp_cutoff(cphs, cutoff):
+    plt.plot(Ts[:-1], cphs[0], color= 'blue', label='lower package')
+    plt.plot(Ts[:-1], cphs[1], color='red', label='higher package')
+    plt.plot(Ts[:-1], np.sum(cphs, axis=0), color='black', ls='dashed', label='total')
+    plt.xlabel(r'Temperature [K]', fontsize=16)
+    plt.ylabel(r'Lattice Heat capacity [J/m$^3$K]', fontsize=16)
+    plt.title(r'cutoff energy=' + str(cutoff * deltE) + 'meV', fontsize=18)
+    plt.legend(fontsize=16)
+    plt.savefig('cp_cutoff_2.pdf')
+    plt.show()
+
+
+#now run the stuff, get the data:
+
+#first for cutoff method:
+E_cutoff= int((35+deltE)/deltE)
+band_list=get_bandstructure('cutoff')
+DOS = get_DOS(band_list, deltE)
+cph_cutoff=cp_cutoff(DOS, E_cutoff)
+
+#then for optical vs. accoustic modes:
+#bands_ac, bands_op=get_bandstructure('opac')
+#DOS_ac= get_DOS(bands_ac, deltE) * deltk / 2 / math.pi / deltE / 1e-3
+#DOS_op= get_DOS(bands_op, deltE) * deltk / 2 / math.pi / deltE / 1e-3
+#cph_opac=cp_opac(DOS_ac, DOS_op)
+#cp_ac=cph_opac[0]
+#cp_op=cph_opac[1]
+
+#plot what you want:
+#plot_bands_DOS('opac', band_list, DOS, bands_ac, bands_op, DOS_ac, DOS_op)
+plot_bands_DOS('cutoff', band_list, DOS, cutoff=E_cutoff)
+#plot_cp_opac(cp_ac, cp_op)
+plot_cp_cutoff(cph_cutoff, E_cutoff)
--- FGT/fgtplot.py
+++ FGT/fgtplot.py
@@ -3,6 +3,8 @@ import h5py
 import scipy
 from scipy import io
 from matplotlib import pyplot as plt
+import os
+import math
 
 
 def templot():
@@ -45,42 +47,43 @@ def simplot(dat):
     te=[float(line[2]) for line in columns]
     tp=[float(line[3]) for line in columns]
     nmag=np.array([float(line[1]) for line in columns])
-    return(times, nmag, te, tp)
+    return(times, np.array(nmag), np.array(te), np.array(tp))
 
 def fitplot():
     tem=templot()
     mag=magplot()
-    sim=simplot('test1.dat')
     sim5=simplot('test5.dat')
-    sim6=simplot('test6.dat')
+    sim6=multiplot('C:/Users/tgrie/OneDrive/Dokumente/Github/3TM_Data/FGT/multilattice_te.dat', 25)
     sim5mag=simplot('test5mag.dat')
-    sim6mag=simplot('test6mag.dat')
+    sim6mag=multiplot('C:/Users/tgrie/OneDrive/Dokumente/Github/3TM_Data/FGT/multilattice_mag.dat', 25)
     koop=simplot('koopmans.dat')
 
 
     fig, (ax1, ax2)=plt.subplots(2,1)
-    ax1.scatter(tem[0]-0.02, tem[1])
-    ax1.plot(tem[0]-0.02, tem[1]-tem[6], color='red', ls='dashed', alpha=0.5)
-    ax1.plot(tem[0]-0.02, tem[1]+tem[6], color='red', ls='dashed', alpha=0.5)
-    ax1.plot(koop[0], koop[2])
+    ax1.scatter(tem[0]-0.015, tem[1])
+    ax1.plot(tem[0]-0.015, tem[1]-tem[6], color='red', ls='dashed', alpha=0.5)
+    ax1.plot(tem[0]-0.015, tem[1]+tem[6], color='red', ls='dashed', alpha=0.5)
+    #ax1.plot(koop[0], koop[2])
     #ax1.plot(sim3[0], sim3[2])
-    ax1.plot(sim5[0], sim5[2])
+    #ax1.plot(sim5[0], sim5[2])
+    ax1.plot(sim6[0], sim6[2], color='purple', label=r'94 J/cm^3')
 
-    ax2.scatter(mag[0], mag[1])
-    ax2.plot(mag[0], mag[1]-mag[2], color='red', ls='dashed', alpha=0.5)
-    ax2.plot(mag[0], mag[1]+mag[2], color='red', ls='dashed', alpha=0.5)
-    ax2.plot(koop[0], koop[1]/0.8975, lw=2.0, label='lit. params')
-    ax2.plot(sim5mag[0], sim5mag[1]/0.8975, label='new params')
-    #ax2.plot(sim6mag[0], sim6mag[1]/0.8975, lw=2.0, color='green', label='higher mag. rate')
+    ax2.scatter(mag[0]+0.12, mag[1])
+    ax2.plot(mag[0]+0.12, mag[1]-mag[2], color='red', ls='dashed', alpha=0.5)
+    ax2.plot(mag[0]+0.12, mag[1]+mag[2], color='red', ls='dashed', alpha=0.5)
+    #ax2.plot(koop[0], koop[1]/0.8975, lw=2.0, label='lit. params')
+    #ax2.plot(sim5mag[0], sim5mag[1]/0.8975, label='new params')
+    ax2.plot(sim6mag[0], sim6mag[1][0]/0.931/1.04, lw=2.0, color='purple', label=r'470 J/cm^3')
 
     ax1.set_ylabel(r'$T_e$ [K]', fontsize=16)
     ax2.set_ylabel(r'$m/m(t=0)$', fontsize=16)
     ax2.set_xlabel(r'delay [ps]', fontsize=16)
 
-    ax1.set_xlim(-1, 5)
-    ax2.set_xlim(-1, 10)
-    plt.legend()
-    plt.savefig('attempt1.pdf')
+    ax1.set_xlim(-0.5, 5)
+    ax2.set_xlim(-0.5, 5)
+    ax1.legend(fontsize=14, bbox_to_anchor = (1., 1.))
+    ax2.legend(fontsize=14, bbox_to_anchor = (0.6, 0.7))
+    plt.savefig('comparison_te_mag.pdf')
     plt.show()
 
 def koopplot():
@@ -113,12 +116,166 @@ def msdplot():
     lines=[i.split('\t') for i in dat]
     times=[float(j[0])-0.8 for j in lines]
     msd=[float(j[1]) for j in lines]
-    plt.xlabel(r'delay [ps]')
-    plt.ylabel(r'normalized MSD [arb. units]')
+    plt.xlabel(r'delay [ps]', fontsize=16)
+    plt.ylabel(r'normalized MSD [arb. units]', fontsize=16)
     return(times, msd)
 
-tptimes, msd= msdplot()
-te=templot()
+def e_lat(tein, t, cp0):
+    cph=cp0*(tein/t)**2*np.exp(tein/t)/(np.exp(tein/t)-1)**2
+    return(cph*t)
 
-plt.scatter(tptimes, msd)
+#multiplot is a function that reads your datafiles and returns arrays of the delays, Nickels' magnetization, electron-/ and lattice temperature of all layers for every timestep. Time Offset in ps
+def multiplot(file, offset):
+    ### open(file, mode) is an in-built function. 1st argument is the filepath, second argument is the mode. Choose mode 'r' to read the desired file
+    input = open(file,'r')
+    content = input.read().split('\n')
+    toplot = [line for line in content if not str(line).startswith('#')]
+    lines = [line.split('\t') for line in toplot]
+    del lines[-1]
+
+    times = np.array([-offset+ float(line[0]) for line in lines])
+
+    mags = [line[1] for line in lines]
+    mag1bae= [mag.replace('[0.0]]', '').replace('[[', '') for mag in mags]
+    mag1=[m.replace('[', '').replace(']', '') for m in mag1bae]
+    m1=[line.split() for line in mag1]
+    m1s=np.array([[float(m1[i][j].replace(',', '')) for i in range(len(m1))] for j in range(len(m1[0]))])
+    ####m1s[i][:] is magnetization for all times of (i-1)th monolayer!###
+    ###m1s[:][i] is magnetization for timestep i of all monolayers!###
+
+    temes = [line[2].split('], [') for line in lines]
+    tebae=[[te[i].replace('[', '').replace(']', '') for i in range(len(temes[0]))] for te in temes]
+    te1bla=[tb[:][0] for tb in tebae]
+
+    te1n=[b.replace(',', '') for b in te1bla]
+    te1m=[te.split() for te in te1n]
+    te1=[[float(i) for i in te] for te in te1m]
+
+
+    tpmes = [line[3].split('], [') for line in lines]
+    tpbae=[[tp[i].replace('[', '').replace(']', '') for i in range(len(tpmes[0]))] for tp in tpmes]
+    tp1bla=[tb[:][0] for tb in tpbae]
+    tp1n=[b.replace(',', '') for b in tp1bla]
+    tp1m=[tp.split() for tp in tp1n]
+    tp1=[[float(i) for i in tp] for tp in tp1m]
+
+
+    tp2s=[line[4].split('], [') for line in lines]
+    tp2bae=[[tp[i].replace('[', '').replace(']', '') for i in range(len(tpmes[0]))] for tp in tp2s]
+    tp21bla=[tb[:][0] for tb in tp2bae]
+    tp21n=[b.replace(',', '') for b in tp21bla]
+    tp21m=[tp.split() for tp in tp21n]
+    tp21=[[float(i) for i in tp] for tp in tp21m]
+    return(np.array(times), np.array(m1s), np.array(te1), np.array(tp1), np.array(tp21))
+
+#fitplot()
+
+def relaxtimes():
+    folderpath='C:/Users/tgrie/OneDrive/Dokumente/GitHub/3TM_Data/FGT/powertests'
+    folderpathbla='C:/Users/tgrie/OneDrive/Dokumente/GitHub/3TM_Data/FGT/powertests/'
+    files=os.listdir(folderpath)
+    taue=[]
+    taup=[]
+
+    for file in files:
+        tecond=5e-3
+        tpcond=5e-4
+        t, m, te, tp=simplot(folderpathbla + str(file))
+        
+        ten=np.roll(te, -1)
+        tpn=np.roll(tp,-1)
+        tpos= t>0
+        t=t[tpos]
+
+        terel=np.where(abs(te[tpos]-ten[tpos])<tecond)
+        tprel=np.where(abs(tp[tpos]-tpn[tpos])<tpcond)
+        #terel=np.where(abs(te[tpos]-te[-1])<tecond)
+        #tprel=np.where(abs(tp[tpos]-tp[-1])<tpcond)
+        pp=float(str(file).replace('new.dat', '').replace('pp', ''))*math.sqrt(2*math.pi)*15e-15*1e-6
+        taue.append([pp, min(t[terel])])
+        taup.append([pp, min(t[tprel])])
+        
+    taue=np.array(taue)
+    taup=np.array(taup)
+    pps=[taue[i,0] for i in range(len(taue))]
+    taues=[taue[i,1] for i in range(len(taue))]
+    taups=[taup[i,1] for i in range(len(taup))]
+
+    #plt.scatter(pps, taues, label=r'$T_e$')
+    plt.scatter(pps, taups)
+    plt.xlabel(r'abs. energy density [J/cm^3]', fontsize=16)
+    plt.ylabel(r'Lattice relaxation times [ps]', fontsize=16)
+    plt.savefig('relaxtimes.pdf')
+    plt.show()
+
+def teqs():
+    folderpath='C:/Users/tgrie/OneDrive/Dokumente/GitHub/3TM_Data/FGT/cps'
+    folderpathbla='C:/Users/tgrie/OneDrive/Dokumente/GitHub/3TM_Data/FGT/cps/'
+    files=os.listdir(folderpath)
+    teq=[]
+
+    for file in files:
+        t, m, te, tp1, tp2=multiplot(folderpathbla+str(file), 25)
+        tint=abs(t-1.)<1e-3
+        print(t[tint])
+        cp=float(str(file).replace('cp', '').replace('ppte.dat', '').replace('ppmag.dat', ''))/1e6
+        teq.append([cp, te[-1], te[tint]])
+        #plt.plot(t, (e_lat(0.75*460, tp1, 0.1)+e_lat(0.75*460, tp2, 0.9)-40)/113.5, label=r'$\Delta(C_pT_p)$', color='purple')
+        #plt.show()
+
+    teq=np.array(teq)
+    plt.scatter(teq[:,0], teq[:,1], label='delay 20 ps')
+    plt.scatter(teq[:,0], teq[:,2], label='delay 1 ps')
+    plt.xlabel(r'Maximum lattice specific heat [MJ/m^3]', fontsize=16)
+    plt.ylabel(r'Electron temperature at different delays')
+    plt.legend(fontsize=14)
+    plt.savefig('teofcp.pdf')
+    plt.show()
+
+def ks():
+    folderpath='C:/Users/tgrie/OneDrive/Dokumente/Github/3TM_Data/FGT/ks'
+    folderpathbla='C:/Users/tgrie/OneDrive/Dokumente/Github/3TM_Data/FGT/ks/'
+    files=os.listdir(folderpath)
+
+    for file in files:
+        t, m, te, tp1, tp2=multiplot(folderpathbla+str(file), 25)
+        k=float(str(file).replace('k', '').replace('.dat', ''))
+        if 10*k%2==0 or k==0.1 or k==0.05:
+            elat=e_lat(0.75*460, tp1, k)+e_lat(0.75*460, tp2, (1-k))
+            plt.plot(t, (elat-elat[0])/(elat-elat[0])[-1], label='k=' + str(k))
+
+    plt.scatter(np.array(msdplot()[0]), msdplot()[1])
+    plt.xlim(-1, 20)
+    plt.xlabel(r'delay [ps]', fontsize=16)
+    plt.ylabel(r'normalized lattice energy', fontsize=16)
+    plt.legend(fontsize=12)
+    plt.savefig('ks.pdf')
+    plt.show()
+                   
+
+#t, m, te, tp, tp2=multiplot('C:/Users/tgrie/OneDrive/Dokumente/Github/3TM_Data/FGT/multilattice_ep.dat', 25)
+#plt.scatter(msdplot()[0], msdplot()[1])
+#plt.plot(t, (e_lat(0.75*460, tp, 0.1)+e_lat(0.75*460, tp2, 0.9)-40)/44.2, color='red')
+#plt.savefig('multi_latdyn.pdf')
+#plt.show()
+
+#plt.plot(t, te)
+#plt.scatter(tel[0], tel[6])
+#plt.show()
+
+#teqs()
+#fitplot()
+#ks()
+#relaxtimes()
+
+t, m, te, tp, tp2=multiplot('C:/Users/tgrie/Onedrive/Dokumente/Github/3tm_results/10cp90gpp2.dat', 25)
+plt.plot(t, te)
+plt.plot(t, tp)
+plt.plot(t, tp2)
+
+plt.xlabel(r'delays [ps]', fontsize=16)
+plt.ylabel(r'T [K]', fontsize=16)
+plt.xlim(-1, 5)
+plt.legend(['282 J/cm^3'], fontsize=14)
+plt.savefig('blashit.pdf')
 plt.show()
