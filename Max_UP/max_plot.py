08aca72c353de02206824d98c0d1c4033b3a413e
Nilodirf <106656383+Nilodirf@users.noreply.github.com>
2023-01-04 12:00:17 +0100

    dfsa

--- Max_UP/max_plot.py
+++ Max_UP/max_plot.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy import constants as sp
 from matplotlib import pyplot as plt
+import os
+from npy_append_array import NpyAppendArray
 
 def dataplot(plot=False):
     delay_file=open('delays.dat', 'r')
@@ -19,59 +21,31 @@ def dataplot(plot=False):
         plt.show()
     return delays, tes
 
-def simplot(file, offset):
-    ### open(file, mode) is an in-built function. 1st argument is the filepath, second argument is the mode. Choose mode 'r' to read the desired file
-    input = open(file, 'r')
-    content = input.read().split('\n')
-    toplot = [line for line in content if not str(line).startswith('#')]
-    lines = [line.split('\t') for line in toplot]
-    del lines[-1]
+def simplot(sim, offset):
+    parentdir='sims'
+    simdir=sim
 
-    times = np.array([-offset + float(line[0]) for line in lines])
+    directory=os.path.join(parentdir, sim)
+    files=os.listdir(directory)
 
-    mags = [line[1] for line in lines]
-    mag1bae = [mag.replace('[0.0]]', '').replace('[[', '') for mag in mags]
-    mag1 = [m.replace('[', '').replace(']', '') for m in mag1bae]
-    m1 = [line.split() for line in mag1]
-    m1s = np.array([[float(m1[i][j].replace(',', '')) for i in range(len(m1))] for j in range(len(m1[0]))])
-    ####m1s[i][:] is magnetization for all times of (i-1)th monolayer!###
-    ###m1s[:][i] is magnetization for timestep i of all monolayers!###
+    #extract delays
+    delayfile=os.path.join(directory,'delays.npy')
+    delays=np.load(delayfile, mmap_mode='r')
 
-    mags2 = [line[5] for line in lines]
-    mag2bae = [mag.replace('[0.0]', '').replace('[[', '') for mag in mags2]
-    mag2 = [m.replace('[', '').replace(']', '') for m in mag2bae]
-    m2 = [line.split() for line in mag2]
-    m2s = np.array([[float(m2[i][j].replace(',', '')) for i in range(len(m2))] for j in range(len(m2[0]))])
+    #find all the electron temperatures
+    tefiles=[f for f in files if str(f).startswith('tes')]
+    for tef in tefiles:
+        file=os.path.join(directory, tef)
+        tes=np.load(file, mmap_mode='r')
+        print(tes.shape)
+        plt.plot(delays, tes)
 
-    tes=[]
-    temes = [line[2].split('], [') for line in lines]
-    tebae = [[te[i].replace('[', '').replace(']', '') for i in range(len(temes[0]))] for te in temes]
-    for i in range(len(tebae[0])):
-        te1bla = [tb[:][i] for tb in tebae]
-        te1n = [b.replace(',', '') for b in te1bla]
-        te1m = [te.split() for te in te1n]
-        te1 = [[float(i) for i in te] for te in te1m]
-        print(te1)
-        tes.append(te1)
+    plt.show()
 
-    tpmes = [line[3].split('], [') for line in lines]
-    tpbae = [[tp[i].replace('[', '').replace(']', '') for i in range(len(tpmes[0]))] for tp in tpmes]
-    tp1bla = [tb[:][0] for tb in tpbae]
-    tp1n = [b.replace(',', '') for b in tp1bla]
-    tp1m = [tp.split() for tp in tp1n]
-    tp1 = [[float(i) for i in tp] for tp in tp1m]
-
-    tp2s = [line[4].split('], [') for line in lines]
-    tp2bae = [[tp[i].replace('[', '').replace(']', '') for i in range(len(tpmes[0]))] for tp in tp2s]
-    tp21bla = [tb[:][0] for tb in tp2bae]
-    tp21n = [b.replace(',', '') for b in tp21bla]
-    tp21m = [tp.split() for tp in tp21n]
-    tp21 = [[float(i) for i in tp] for tp in tp21m]
-
-    return (np.array(times), np.array(m1s), np.array(tes), np.array(tp1), np.array(tp21), np.array(m2s))
 
 dat_times, dat_tes=dataplot()
-sim_times, sim_ms, sim_te, sim_tp, sim_tp2, sim_m2s=simplot('test2.dat', 0.1)
+simplot('test1', 0.1)
+
 
 #print(sim_te)
 #plt.plot(sim_times, sim_te[0])
